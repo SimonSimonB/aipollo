f28968e606e852f6a30a0c3fe5553c735947e3d7
Simon <erulastantron@gmail.com>
2020-11-20 21:17:50 +0100

    Make web app show the score with the detected objects.

--- aipollo_web/scoreanalyzer/views.py
+++ aipollo_web/scoreanalyzer/views.py
@@ -1,18 +1,29 @@
-from aipollo_processor.detectors import half_note_detector
 import django
-from aipollo_processor import score_analyzer
 import cv2
+import jsonpickle
+
+from aipollo_processor import score_analyzer
 
 
 def index(request):
     template = django.template.loader.get_template('./scoreanalyzer/analyzer.html')
 
-    # Debug: load sheet music scan from disk.
-    image = cv2.imread('../sample_scans/bleib_rotated.jpg', cv2.IMREAD_GRAYSCALE)
-    staffs, half_notes = score_analyzer.analyze_score(image)
+    # Debug: load sheet music scan from disk rather than getting them from the POST request.
+    from django.contrib.staticfiles import finders
+    path_to_image = finders.find('scoreanalyzer/bleib_rotated.jpg')
+    image = cv2.imread(path_to_image, cv2.IMREAD_GRAYSCALE)
+
+    # IF Debug
+    path_to_json = finders.find('scoreanalyzer/debug_score_elements.json')
+    with open(path_to_json, 'r') as f:
+        score_elements_json = f.read()
+    # ELSE
+    #score_elements = score_analyzer.analyze_score(image)
+    #score_elements_json = jsonpickle.encode(score_elements, unpicklable=False)
+
+
     context = {
-        'staffs': staffs,
-        'half_notes': half_notes,
+        'score_elements_json': score_elements_json,
     }
     return django.http.HttpResponse(template.render(context, request))
     
