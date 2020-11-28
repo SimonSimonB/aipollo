d5c32d5aa8508293055dbfcea27ad3911744aa2a
Simon <erulastantron@gmail.com>
2020-11-28 14:26:09 +0100

    Minor style improvements in HTML, CSS, and JavaScript files

--- aipollo_web/scoreanalyzer/static/scoreanalyzer/interactive_annotation.js
+++ aipollo_web/scoreanalyzer/static/scoreanalyzer/interactive_annotation.js
@@ -1,23 +1,26 @@
-const POINT_RADIUS = 1
-const COLORS = { 'half_note': 'red', 'quarter_note': 'green' }
-
 function init(scoreElementsJson) {
+    const POINT_RADIUS = 1;
+    const COLORS = { 'half_note': 'red', 'quarter_note': 'green' };
+
+    // Render notes, breaks, etc.
     scoreElements = renderScoreElements(scoreElementsJson);
-    var totalPositionChange = new WeakMap()
+
+    // Set up the dictionary to save how much the user moved an element around.
+    let totalPositionChange = new WeakMap();
     for (const scoreElementSvg of scoreElements) {
-        totalPositionChange.set(scoreElementSvg, { x: 0, y: 0 })
+        totalPositionChange.set(scoreElementSvg, { x: 0, y: 0 });
     }
 
     function renderScoreElements(scoreElementsJsonParsed) {
-        var scoreElements = []
+        let scoreElements = [];
         for (const scoreElement of scoreElementsJsonParsed) {
-            elementType = scoreElement.type._name_
+            elementType = scoreElement.type._name_;
 
             // Don't draw staff lines.
             if (elementType == 'staff') {
                 continue;
             }
-            var scoreElementSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+            let scoreElementSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
             scoreElementSvg.classList.add('draggable');
             scoreElementSvg.classList.add('scoresymbol');
 
@@ -33,28 +36,29 @@ function init(scoreElementsJson) {
 
             // Create a SVG circle for each pixel belonging to the score element.
             for (const pixel of scoreElement.pixels) {
-                var circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+                let circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
                 circle.setAttribute('cy', pixel.y - minY);
                 circle.setAttribute('cx', pixel.x - minX);
                 circle.setAttribute('r', POINT_RADIUS);
-                scoreElementSvg.appendChild(circle)
+                scoreElementSvg.appendChild(circle);
 
-                circle.style.fill = COLORS[elementType]
-                circle.style.fillOpacity = 0.3
+                circle.style.fill = COLORS[elementType];
+                circle.style.fillOpacity = 0.3;
             }
 
-            scoreElements.push(scoreElementSvg)
-            scoreElements.concat(renderScoreElements(scoreElement.children))
+            scoreElements.push(scoreElementSvg);
+            scoreElements.concat(renderScoreElements(scoreElement.children));
             document.getElementById('imagediv').appendChild(scoreElementSvg);
         }
 
-        return scoreElements
+        return scoreElements;
     }
 
+    // This code permits users to drag and drop elements.
     interact('.draggable').draggable({
         listeners: {
             start(event) {
-                console.log(event.type, event.target)
+                console.log(event.type, event.target);
             },
             move(event) {
                 movedId = event.target;
@@ -62,7 +66,7 @@ function init(scoreElementsJson) {
                 totalPositionChange.get(movedId).y += event.dy;
 
                 event.target.style.transform =
-                    `translate(${totalPositionChange.get(movedId).x}px, ${totalPositionChange.get(movedId).y}px)`
+                    `translate(${totalPositionChange.get(movedId).x}px, ${totalPositionChange.get(movedId).y}px)`;
             },
         }
     })
